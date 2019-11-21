7c581e8e1c61b710d9f74d4580e48dc2c54d267f
Ryan Bahan <ryan@ryanbahan.com>
2019-11-21 08:58:23 -0700

    Add transactions page

--- main.js
+++ main.js
@@ -6,3 +6,22 @@ closeButton.addEventListener("click", closeBanner);
 function closeBanner() {
   welcomeBanner.style.display = "none";
 }
+
+const transactionsNav = document.getElementById('transactions-nav');
+const mainPageContent = document.getElementById('main');
+const homeNav = document.getElementById('home-nav');
+const transactionsPage = document.getElementById('full-transactions');
+
+transactionsNav.addEventListener('click', displayTransactions)
+
+function displayTransactions() {
+  mainPageContent.style.display = "none";
+  transactionsPage.style.display = "block";
+}
+
+homeNav.addEventListener('click', displayHome)
+
+function displayHome() {
+  main.style.display = "block";
+  transactionsPage.style.display = "none";
+}
